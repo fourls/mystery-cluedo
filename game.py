bf248be78b3c344d690c4084165674811f438287
rofwell <elliot@peers.net.au>
2017-06-11 00:30:29 +1000

    Fixed a mild bug in game.py

--- game.py
+++ game.py
@@ -71,7 +71,7 @@ while 1:
             for personSprite in peopleGroup:
                 if personSprite.rect.collidepoint((mx,my)):
                     for match in fw.askPerson(personSprite.person.name,raw_input('who: '),raw_input('what: '),raw_input('where: '),raw_input('when: ')):
-                        print(person.name + ' says they saw ' + match['who'] + ' ' + match['what'] + ' the ' + match['where'] + ' at ' + str(match['when']))
+                        print(personSprite.person.name + ' says they saw ' + match['who'] + ' ' + match['what'] + ' the ' + match['where'] + ' at ' + str(match['when']))
 
     screen.fill((0,0,0))
     roomGroup.draw(screen)
