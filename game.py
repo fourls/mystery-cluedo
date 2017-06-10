02850df7dd7edc8b73efea219fbaaa4594df719d
rofwell <elliot@peers.net.au>
2017-06-10 12:56:35 +1000

    Imported framework into game.py

--- game.py
+++ game.py
@@ -1,24 +1,33 @@
 import sys, pygame
+import framework as fw
 pygame.init()
 
 SIZE = WIDTH, HEIGHT = 320, 240
 FPS = 40
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode(SIZE)
-pygame.display.set_caption('hey look its a box')
+pygame.display.set_caption('mystery cluedo')
 
 class Person(pygame.sprite.Sprite):
 
-    def __init__(self, color):
+    def __init__(self, name, x, y):
        pygame.sprite.Sprite.__init__(self)
-
-       self.image = pygame.image.load('img/' + color + '.png')
+       self.name = name
+       self.image = pygame.image.load('img/' + name + '.png')
        self.rect = self.image.get_rect()
+       self.rect.x, self.rect.y = x, y
 
 
 peopleGroup = pygame.sprite.Group()
-personSprite = Person('green')
-peopleGroup.add(personSprite)
+
+x = 10
+y = 10
+for person in fw.people:
+    personSprite = Person(person.name, x, y)
+    peopleGroup.add(personSprite)
+
+    x += 30
+    y += 30
 
 while 1:
     for event in pygame.event.get():
