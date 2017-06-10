7a58ee6d25e5d02e8a6c86d52c08e5ec28957276
rofwell <elliot@peers.net.au>
2017-06-10 20:37:10 +1000

    Added text describing each room

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ pygame.init()
 
 SIZE = WIDTH, HEIGHT = 400, 400
 FPS = 40
+FONT = pygame.font.SysFont("Arial", 10)
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode(SIZE)
 pygame.display.set_caption('mystery cluedo')
@@ -23,6 +24,13 @@ class Room(pygame.sprite.Sprite):
         self.image = pygame.image.load('img/room.png')
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = x, y
+
+        if self.rect.x % 200 != 0:
+            self.image = pygame.image.load('img/room-alt.png')
+
+        textSurf = FONT.render(self.room.name, 1, (0,0,0))
+        textRect = textSurf.get_rect(center=(self.rect.width/2, self.rect.height/2))
+        self.image.blit(textSurf, textRect)
 
 fw.initialise()
 
@@ -45,14 +53,11 @@ for person in fw.people:
     px = 300
     py = 300
     for roomSprite in roomGroup:
-        print(person.name + ' in ' + roomSprite.room.name + '? roomSprite.room.people = ' + str(roomSprite.room.people))
         for p2 in roomSprite.room.people:
             if p2['name'] == person.name:
                 px = roomSprite.rect.x + ((roomSprite.room.people.index(p2)) * 20)
-                py = roomSprite.rect.y + ((roomSprite.room.people.index(p2)) * 20)
-                print(person.name + ' is in ' + roomSprite.room.name)
+                py = roomSprite.rect.y + 20 + ((roomSprite.room.people.index(p2)) * 20)
     
-    print('x = ' + str(px) + ', y = ' + str(py))
     personSprite = Person(person, px, py)
     peopleGroup.add(personSprite)
 
